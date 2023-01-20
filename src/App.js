d2016a806141b1cfcc884f91cc97cde69d7cf326
Salvatore Vella <salvatorevella1234@gmail.com>
2023-01-20 10:20:46 +0100

    Bozza pagina film

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import MoviePage from "./MoviePage";
 import SearchForm from "./SearchForm";
 
 function App() {
@@ -5,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <SearchForm />
-      
+      <MoviePage />
     </div>
   );
 }
--- src/MoviePage.js
+++ src/MoviePage.js
@@ -1,19 +1,25 @@
+import { useEffect, useState } from "react";
+
 function MoviePage() {
-    const options = {
-      method: 'GET',
-      headers: {
-        'X-RapidAPI-Key': '18af9ee291msh70a301a734cd7fap13f07ejsn57e8b52159f6',
-        'X-RapidAPI-Host': 'streaming-availability.p.rapidapi.com'
-      }
-    };
-    
-    fetch('https://streaming-availability.p.rapidapi.com/get/basic?country=it&imdb_id=%20&output_language=en', options)
-      .then(response => response.json())
-      .then(response => console.log(response))
-      .catch(err => console.error(err));
-    
+const [film, setFilm] = useState([]);
+useEffect(() => {
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': '18af9ee291msh70a301a734cd7fap13f07ejsn57e8b52159f6',
+      'X-RapidAPI-Host': 'streaming-availability.p.rapidapi.com'
+    }
+  };
+  
+  fetch('https://streaming-availability.p.rapidapi.com/get/basic?country=it&imdb_id=tt14452776&output_language=en', options)
+    .then(response => response.json())
+    .then(response => setFilm(() => response))
+    .catch(err => console.error(err));
+}, [])
+    useEffect(() => console.log(film), [film])
     return (
-      <div className="App">
+      <div className="App dark:bg-black">
+        <img className="w-full sm:w-1/2 dark:border-white dark:border-4" src={film.backdropURLs.original}/>
       </div>
     );
   }
