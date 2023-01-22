5a1ee887744b24fead5b2691f3e8364180724bec
Salvatore Vella <salvatorevella1234@gmail.com>
2023-01-22 14:16:58 +0100

    Search better

--- src/MovieCard.js
+++ src/MovieCard.js
@@ -2,7 +2,9 @@ export default function MovieCard({ img, title, id, rating, duration, genres }) 
 
     return <div className="w-60 cursor-pointer overflow-hidden bg-black dark:bg-gray-200 dark:shadow-gray-600
     dark:shadow-md dark:border dark:border-white text-center rounded-md m-3
-    border border-gray-600 text-white dark:text-black shadow-xl">
+    border border-gray-600 text-white dark:text-black drop-shadow-2xl
+    hover:brightness-50 hover:scale-105 transition-all duration-250
+    active:scale-95">
         <h3 className="text-md px-2 py-2 font-bold truncate">{title}</h3>
         <img className="h-84 w-full" src={img} alt={title} key={id}></img>
         <div className="flex justify-between px-2">
--- src/SearchForm.js
+++ src/SearchForm.js
@@ -8,7 +8,7 @@ export default function SearchForm() {
     async function search(e) {
         e.preventDefault();
         try {
-            const json = await fetch(`https://imdb-api.com/API/AdvancedSearch/k_8p9jb7w1?title=${e.target.elements.title.value}&title_type=feature,tv_movie,tv_series,tv_special,tv_miniseries,documentary,short,&user_rating=1.0,10&moviemeter=30,3000&`);
+            const json = await fetch(`https://imdb-api.com/API/AdvancedSearch/k_8p9jb7w1?title=${e.target.elements.title.value}`);
             const data = await json.json();
             setInfos(() => data.results);
         } catch (err) {
