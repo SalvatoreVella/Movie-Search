4d6e213c45d221bd3dc99b668b72bc78fbf9358e
Chiara Arrivas <ritarrivas96@gmail.com>
2023-01-22 18:40:19 +0100

    cose

--- src/MoviePage.js
+++ src/MoviePage.js
@@ -1,27 +1,33 @@
 import { useEffect, useState } from "react";
 
 function MoviePage() {
-const [film, setFilm] = useState([]);
-useEffect(() => {
-  const options = {
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Key': '18af9ee291msh70a301a734cd7fap13f07ejsn57e8b52159f6',
-      'X-RapidAPI-Host': 'streaming-availability.p.rapidapi.com'
-    }
-  };
-  
-  fetch('https://streaming-availability.p.rapidapi.com/get/basic?country=it&imdb_id=tt14452776&output_language=en', options)
-    .then(response => response.json())
-    .then(response => setFilm(() => response))
-    .catch(err => console.error(err));
-}, [])
-    useEffect(() => console.log(film), [film])
-    return (
-      <div className="App dark:bg-black">
-        <img className="w-full sm:w-1/2 dark:border-white dark:border-4" src={film.backdropURLs.original}/>
-      </div>
+  const [film, setFilm] = useState([]);
+  useEffect(() => {
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': '18af9ee291msh70a301a734cd7fap13f07ejsn57e8b52159f6',
+        'X-RapidAPI-Host': 'streaming-availability.p.rapidapi.com'
+      }
+    };
+
+    fetch('https://streaming-availability.p.rapidapi.com/get/basic?country=it&imdb_id=tt14452776&output_language=en', options)
+      .then(response => response.json())
+      .then(response => setFilm(() => response))
+      .catch(err => console.error(err));
+  }, [])
+  useEffect(() => console.log(film), [film])
+  return (
+<div>
+    {film && <div className="App dark:bg-black">
+      <h2 className="w-full">{film.originalTitle}</h2>
+      <img className="w-full" src={film.backdropURLs?.original} />
+      <p className="w-full">https://www.youtube.com/{film.video}</p>
+      <p className="w-full">{film.episodes}</p>
+      <p className="w-full">{film.overview}</p>
+    </div>}
+</div>  
     );
   }
-  
-  export default MoviePage;
+
+export default MoviePage;
